f54eac6b4284accbfc6adb2f9b96c8bdb4945c8d
dima-zharyy <zharyydimka@gmail.com>
2022-11-05 16:46:08 +0200

    added home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 // import Head from "next/head";
 // import Image from "next/image";
+import { MoviesList } from "../components";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { getTrending } from "../service/api";
+import styles from "../styles/Home.module.css";
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  const movies = await getTrending();
 
-export default function Home() {
-  return <h1>Home Page</h1>;
+  return {
+    props: { movies }, // will be passed to the page component as props
+  };
+};
+
+export default function Home({
+  movies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Trending today</h1>
+      {movies && <MoviesList movies={movies.results} />}
+    </div>
+  );
 }
